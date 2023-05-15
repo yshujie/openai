52e19b711d8c5a7852115fcbd899207ad93ed41a
yangshujie <yangshujie@fangcunyisheng.com>
2023-05-15 13:26:54 +0800

    fix completed marshaller

--- common/marshaller.go
+++ common/marshaller.go
@@ -1,1 +1,15 @@
 package common
+
+import "encoding/json"
+
+// marshaller 数据序列化程序
+type marshaller interface {
+	marshal(v any) ([]byte, error)
+}
+
+// jsonMarshaller json 数据序列化程序
+type jsonMarshaller struct{}
+
+func (jm *jsonMarshaller) marshal(v any) ([]byte, error) {
+	return json.Marshal(v)
+}
