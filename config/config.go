3b3e38a7eafd43e1b7115ef669333f8f3038abcb
yangshujie <yangshujie@fangcunyisheng.com>
2023-05-15 14:09:53 +0800

    fix completed config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,26 @@
 package config
+
+const openaiAPIURLv1 = "https://api.openai.com/v1"
+
+type APIType string
+
+const APITypeOpenAI APIType = "OPEN_AI"
+
+type ClientConfig struct {
+	BaseUrl string
+	APIType APIType
+	APIKey  string
+}
+
+// DefaultConfig 默认的客户端配置
+func DefaultConfig(APIKey string) ClientConfig {
+	return ClientConfig{
+		BaseUrl: openaiAPIURLv1,
+		APIType: APITypeOpenAI,
+		APIKey:  APIKey,
+	}
+}
+
+func (ClientConfig) string() string {
+	return "<OpenAI API ClientConfig>"
+}
